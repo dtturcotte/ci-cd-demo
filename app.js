53eee86dd167713e2754b3c6f6352fafa5123220
Daniel Turcotte <dthusky@gmail.com>
2024-05-07 18:18:00 -0400

    test

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 8081
 
 app.get('/', (req, res) => {
 	res.send('Hello CICD World!')
-})
-
-app.listen(port, '0.0.0.0', () => {
-	console.log(`Server running on http://localhost:${port}`)
 })
 
 module.exports = app
--- app.test.js
+++ app.test.js
@@ -1,17 +1,16 @@
 const request = require('supertest')
 const app = require('./app')
+let server
+
+beforeAll((done) => {
+	server = app.listen(8081, done) // Use a different or dynamically assigned port if needed
+})
+
+afterAll((done) => {
+	server.close(done)
+})
 
 describe('GET /', () => {
-	let server
-
-	beforeAll((done) => {
-		server = app.listen(8080, done)
-	})
-
-	afterAll((done) => {
-		server.close(done)
-	})
-
 	test('It should respond with "Hello CICD World!"', async () => {
 		const response = await request(server).get('/')
 		expect(response.statusCode).toBe(200)
