1027755ba1b1271679dfc740edeeb252054a68ff
Daniel Turcotte <dthusky@gmail.com>
2024-05-07 17:51:03 -0400

    test

--- app.test.js
+++ app.test.js
@@ -2,8 +2,18 @@ const request = require('supertest')
 const app = require('./app')
 
 describe('GET /', () => {
+	let server
+
+	beforeAll((done) => {
+		server = app.listen(8080, done)
+	})
+
+	afterAll((done) => {
+		server.close(done)
+	})
+
 	test('It should respond with "Hello CICD World!"', async () => {
-		const response = await request(app).get('/')
+		const response = await request(server).get('/')
 		expect(response.statusCode).toBe(200)
 		expect(response.text).toContain('Hello CICD World!')
 	})
