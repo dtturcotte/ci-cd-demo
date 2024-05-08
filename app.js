a94b89eda3531fee273fec25a584c2c8a2253c30
Daniel Turcotte <dthusky@gmail.com>
2024-05-07 20:32:17 -0400

    est

--- app.js
+++ app.js
@@ -5,4 +5,12 @@ app.get('/', (req, res) => {
 	res.send('Hello CICD World!')
 })
 
+app.get('/hello', (req, res) => {
+	res.send('Hello CICD World!')
+})
+
+app.get('/world', (req, res) => {
+	res.send('Hello CICD World!')
+})
+
 module.exports = app
